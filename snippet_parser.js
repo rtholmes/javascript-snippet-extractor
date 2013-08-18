ba4c4b095717692c02e7f768c5be85ec87db8f46
siddhukrs <siddhukrs@localhost>
2013-08-18 18:53:55 -0400

    sniipet 1.2

--- snippet_parser.js
+++ snippet_parser.js
@@ -112,8 +112,8 @@ function analyzeCode(code)
 	var res1 = jsonpath(ast, "$.body[0]..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"VALUE"});
 	var res2 = jsonpath(ast, "$.body[0]..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"PATH"});*/	
 
-	var res1 = jsonpath(ast, "$.body[0]..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"VALUE"});
-	var res2 = jsonpath(ast, "$.body[0]..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"PATH"});	
+	var res1 = jsonpath(ast, "$..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"VALUE"});
+	var res2 = jsonpath(ast, "$..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"PATH"});	
 	console.log('done 3');
 
 	for(var item in res2)
