b00d7558d6f0ec78c1959a636919faa06d669f56
siddhukrs <siddhukrs@localhost>
2013-08-18 18:11:51 -0400

    extend 1.12

--- parser.js
+++ parser.js
@@ -110,379 +110,8 @@ function traverse(node, performAtNode, itemsVisited)
 
 
 var performAtNode = function(node)
-{/*
-	//console.log(itemsVisited.length);
-	if(node.hasOwnProperty('type') === false)
-	{
-		return;
-	}
-	else if (node.type === 'FunctionDeclaration') 
-	{
-			//console.log('f-f-f-f-f-f-f---------------',node.id.name, ':', node.params);
-			var newFunc = {};
-			for(var key in node)
-			{
-				if (node.hasOwnProperty(key))
-				{	
-					if(key !== 'body')
-						newFunc[key]=node[key];
-				}
-			}
-			//console.log(newFunc);
-			knownFunctions[newFunc.id.name]=newFunc;
-			
-		}
-		else if(node.type === 'AssignmentExpression')
-		{
-			if(node.right.hasOwnProperty('type') === false)
-			{
-				//return;
-			}
-			else if(node.right.type === 'AssignmentExpression')
-			{
-				assignmentChain[assignmentChain.length] = node.left;
-			}
-
-			else if(node.right.type === 'FunctionExpression')    
-			{
-				//if(node.left.type === 'MemberExpression')
-					//console.log('f-f-f-f-f-f-f---------------',node.left.object.name, '.', node.left.property.name, ':', node.right.params);
-					//console.log(JSON.stringify(node.left));
-					//console.log(JSON.stringify(node.right));
-					var newFunc = {};
-					var obj = {};
-					obj['type'] = 'Identifier';
-					for(var key in node.right)
-					{
-						if (node.right.hasOwnProperty(key))
-						{	
-							if(key !== 'body')
-								newFunc[key]=node.right[key];
-						}
-					}
-					
-					if(node.left.hasOwnProperty('type') === false)  
-					{
-						//return;
-					}
-					else if(node.left.type === 'MemberExpression')    
-					{
-						
-						obj['name'] = visitMemberExpression(node.left, []);
-						newFunc.id=obj;
-						//console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% '+ newFunc.id.name);
-						knownFunctions[newFunc.id.name]=newFunc;
-					}
-					else if(node.left.type === 'Identifier')    
-					{
-						obj['name'] = node.left.name;
-						newFunc.id=obj;
-						knownFunctions[newFunc.id.name]=newFunc;
-					}
-					
-					for(var j=0; j<assignmentChain.length;j++)
-					{
-						//console.log(JSON.stringify(assignmentChain[j])+'+++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-						if(assignmentChain[j] != undefined || assignmentChain[j].hasOwnProperty('type') === false)
-						{
-
-						}
-						else if(assignmentChain[j].type === 'MemberExpression')    
-						{	
-							obj['name'] = visitMemberExpression(assignmentChain[j], []);
-							newFunc.id=obj;
-							knownFunctions[newFunc.id.name]=newFunc;
-						}
-						else if(assignmentChain[j].type === 'Identifier')
-						{
-							obj['name']= assignmentChain[j].name;
-							newFunc.id=obj;
-							knownFunctions[newFunc.id.name]=newFunc;
-						}
-						else if(assignmentChain[j].type === 'VariableDeclarator')
-						{
-							obj['name']= assignmentChain[j].id.name;
-							newFunc.id=obj;
-							knownFunctions[newFunc.id.name]=newFunc;
-						}
-					}
-					//console.log(newFunc);
-					assignmentChain=[];
-				}
-
-				else if(node.right.type==='Identifier')
-				{
-					if(knownFunctions.hasOwnProperty(node.right.name))
-					{
-						var newFunc = knownFunctions[node.right.name];
-						var obj = {};
-						obj['type'] = 'Identifier';
-						if(node.left.hasOwnProperty('type') === false)
-						{
-
-						}
-						else if(node.left.type === 'MemberExpression')    
-						{
-							obj['name'] = visitMemberExpression(node.left, []);
-							newFunc.id=obj;
-							knownFunctions[newFunc.id.name]=newFunc;
-						}
-						else if(node.left.type === 'Identifier')    
-						{	
-							obj['name'] = node.left.name;
-							newFunc.id=obj;
-							knownFunctions[newFunc.id.name]=newFunc;
-						}
-						
-
-						for(var j=0; j<assignmentChain.length;j++)
-						{
-							//console.log(JSON.stringify(assignmentChain[j])+'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-							if(assignmentChain[j].hasOwnProperty('type') === false)
-							{
-
-							}
-							else if(assignmentChain[j].type === 'MemberExpression')    
-							{
-								obj['name'] = visitMemberExpression(assignmentChain[j],[]);
-								newFunc.id=obj;
-								knownFunctions[newFunc.id.name]=newFunc;
-								
-							}
-							else if(assignmentChain[j].type === 'Identifier')
-							{
-								obj['name']= assignmentChain[j].name;
-								newFunc.id=obj;
-								knownFunctions[newFunc.id.name]=newFunc;
-							}
-							else if(assignmentChain[j].type === 'VariableDeclarator')
-							{
-								obj['name']= assignmentChain[j].id.name;
-								newFunc.id=obj;
-								knownFunctions[newFunc.id.name]=newFunc;
-							}
-						}
-					}
-					assignmentChain=[];
-				}
-				else if(node.right.type === 'ObjectExpression')
-				{
-					//console.log('*********************************************************************************************');
-					var id;
-					if(node.left.hasOwnProperty('type') === false)
-					{
-
-					}
-					else if(node.left.type === 'MemberExpression')    
-					{
-						id = visitMemberExpression(node.left, []);
-					}
-					else if(node.left.type === 'Identifier')    
-					{
-						id = node.left.name;
-					}
-					
-					for(var i=0; i<node.right.properties.length;i++)
-					{
-						var property = node.right.properties[i];
-						if(property.value.hasOwnProperty('type') === false)
-						{
-							//return;
-						}
-						else if(property.value.type === 'Identifier')
-						{
-							if(knownFunctions.hasOwnProperty(property.value.name))
-							{
-								var newFunc = knownFunctions[property.value.name];
-								var obj = {};
-								obj['type'] = 'Identifier';
-								obj['name'] = id + '.' + property.key.name;
-								newFunc.id = obj;
-								knownFunctions[newFunc.id.name] = newFunc;
-								for(var j=0; j<assignmentChain.length;j++)
-								{
-										//console.log(JSON.stringify(assignmentChain[j])+'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-										if(assignmentChain[j].hasOwnProperty('type') === false)
-										{
-
-										}
-										else if(assignmentChain[j].type === 'MemberExpression')    
-										{
-											obj['name'] = visitMemberExpression(assignmentChain[j], [])+'.'+property.key.name;
-											newFunc.id=obj;
-											knownFunctions[newFunc.id.name]=newFunc;
-										}
-										else if(assignmentChain[j].type === 'Identifier')
-										{
-											obj['name']= assignmentChain[j].name+'.'+property.key.name;
-											newFunc.id=obj;
-											knownFunctions[newFunc.id.name]=newFunc;
-										}
-										else if(assignmentChain[j].type === 'VariableDeclarator')
-										{
-											obj['name']= assignmentChain[j].id.name;
-											newFunc.id=obj;
-											knownFunctions[newFunc.id.name]=newFunc;
-										}
-										
-									}
-								}
-								else
-								{
-									nonFunctionProperties[nonFunctionProperties.length] = id + '.' + property.key.name;
-								}
-							}
-							else if (property.value.type === 'FunctionExpression') 
-							{
-								var newFunc = {};
-								for(var key in property.value)
-								{
-									if (property.value.hasOwnProperty(key))
-									{	
-										if(key !== 'body')
-											newFunc[key]=node.right[key];
-									}
-								}
-								var obj = {};
-								obj['type'] = 'Identifier';
-								obj['name'] = id+'.' + property.key.name;
-								newFunc.id=obj;
-								//newFunc.id['name'] = node.left.object.name+'.'+node.left.property.name;
-								knownFunctions[newFunc.id.name]=newFunc;
-								for(var j=0; j<assignmentChain.length;j++)
-								{
-									//console.log(JSON.stringify(assignmentChain[j])+'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-									var obj = {};
-									obj['type'] = 'Identifier';
-									if(assignmentChain[j] === undefined || assignmentChain[j].hasOwnProperty('type') === false) 
-									{
-
-									}
-									else if(assignmentChain[j].type === 'MemberExpression')    
-									{
-										obj['name'] = visitMemberExpression(assignmentChain[j],[])+'.'+property.key.name;
-										newFunc.id=obj;
-										knownFunctions[newFunc.id.name]=newFunc;
-									}
-									else if(assignmentChain[j].type === 'Identifier')
-									{
-										obj['name']= assignmentChain[j].name+'.'+property.key.name;
-										newFunc.id=obj;
-										knownFunctions[newFunc.id.name]=newFunc;
-									}
-									else if(assignmentChain[j].type === 'VariableDeclarator')
-									{	
-										obj['name']= assignmentChain[j].id.name;
-										newFunc.id=obj;
-										knownFunctions[newFunc.id.name]=newFunc;
-									}
-									//newFunc.id=obj;
-									//knownFunctions[newFunc.id.name]=newFunc;
-								}
-							}
-
-						}
-						assignmentChain=[];
-					}
-				}
-				else if(node.type === 'VariableDeclaration')
-				{
-					//console.log(node.declarations.length);
-					for(var k=0; k<node.declarations.length;k++)
-					{
-						var declaration = node.declarations[k];
-						//console.log(i+ ' ' +declaration.id.name);
-						if(declaration.hasOwnProperty('init') === false || declaration.init === null || declaration.init.hasOwnProperty('type') === false)
-						{
-
-						}
-						else if(declaration.init.type === 'AssignmentExpression')
-						{
-							var obj = {};
-							//obj.type = declaration.type;
-							//obj.id = declaration.id;
-							for(var key2 in declaration)
-							{
-								if (declaration.hasOwnProperty(key2))
-								{
-									if(key!== 'init')	
-										obj[key2] = declaration[key2];
-								}
-							}
-							assignmentChain[assignmentChain.length] = obj;
-						}
-						else if(declaration.init.type === 'FunctionExpression')
-						{
-							var newFunc = {};
-							for(var key in declaration.init)
-							{
-								if (declaration.init.hasOwnProperty(key))
-								{	
-									if(key !== 'body')
-										newFunc[key]=declaration.init[key];
-								}
-							}
-							var obj = {};
-							obj['type'] = 'Identifier';
-							obj['name']= declaration.id.name;
-							newFunc.id = obj;
-							knownFunctions[newFunc.id.name]=newFunc;
-							//console.log(newFunc);
-							//console.log('fu-fu-fu-fu-fu-fu-fu---------------',declaration.id.name, ':', declaration.init.params);
-							assignmentChain=[];
-						}
-						else if(declaration.init.type === 'ObjectExpression')
-						{
-							var id = declaration.id.name;
-							//console.log(JSON.stringify(declaration) + '^^^^^^^^^^^^^^^^^^^^');
-							for(var i=0; i<declaration.init.properties.length;i++)
-							{
-								var property = declaration.init.properties[i];
-								if(property.value.hasOwnProperty('type') === false)
-								{
-
-								}
-								else if(property.value.type === 'Identifier')
-								{
-									if(knownFunctions.hasOwnProperty(property.value.name))
-									{
-										var newFunc = knownFunctions[property.value.name];
-										var obj = {};
-										obj['type'] = 'Identifier';
-										obj['name'] = id + '.' + property.key.name;
-										newFunc.id = obj;
-										knownFunctions[newFunc.id.name] = newFunc;
-									}
-									else
-									{
-										nonFunctionProperties[nonFunctionProperties.length] = id + '.' + property.key.name;
-									}
-								}
-								else if (property.value.type === 'FunctionExpression') 
-								{
-									var newFunc = {};
-									for(var key in property.value)
-									{
-										if (property.value.hasOwnProperty(key))
-										{	
-											if(key !== 'body')
-												newFunc[key]=property.value[key];
-										}
-									}
-									var obj = {};
-									obj['type'] = 'Identifier';
-									obj['name'] = id+'.' + property.key.name;
-									newFunc.id=obj;
-									//newFunc.id['name'] = node.left.object.name+'.'+node.left.property.name;
-									knownFunctions[newFunc.id.name]=newFunc;
-								}
-							}
-							assignmentChain=[];
-						}
-
-					}
-
-				}
+{
+	
 				else if(node.type === 'MemberExpression')    
 				{
 					id = visitMemberExpression(node,[]);
@@ -497,7 +126,7 @@ var performAtNode = function(node)
 						nonFunctionProperties[nonFunctionProperties.length]=id;
 					
 				}
-	*/}
+	}
 
 			function append(array, value)
 			{
@@ -676,8 +305,6 @@ var performAtNode = function(node)
 								if(node.right.type === 'AssignmentExpression')
 								{
 									assignmentChain[assignmentChain.length] = node.left;
-									//var nameOfME = visitMemberExpression(node.left, []);
-									//console.log('---AE--'+nameOfME);
 								}
 								else if(node.left.type === 'MemberExpression')    
 								{
@@ -699,10 +326,19 @@ var performAtNode = function(node)
 							}
 							if(node.type === 'VariableDeclarator')
 							{
-								var tempArray = [];
-								tempArray[0]=node.id.name;
-								leftNodes = append(leftNodes, tempArray);
-								console.log('---'+node.id.name);
+								if(node.init.type === 'AssignmentExpression')
+								{
+									assignmentChain[assignmentChain.length] = node.id;
+								}
+								else
+								{
+									assignmentChain[assignmentChain.length] = node.id;
+									var assignmentArray = [];
+									assignmentArray = getAssignmentChain(assignmentChain);
+									leftNodes = append(leftNodes, assignmentArray);
+									assignmentChain = [];
+									console.log('---'+node.id.name);
+								}
 							}
 							if(node.type === 'Property')
 							{
