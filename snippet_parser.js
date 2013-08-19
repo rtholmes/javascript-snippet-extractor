db4b69a4c958f2ceb9e2feb8fc55b573bc1c0039
siddhukrs <siddhukrs@localhost>
2013-08-19 01:53:05 -0400

    snippet parser update 2.8

--- snippet_parser.js
+++ snippet_parser.js
@@ -152,7 +152,7 @@ function analyzeCode(code)
 	var ast = esprima.parse(code);
 	var identifiedMethods = [];
 	var jsonpath = require('JSONPath').eval;
-
+	var methodCalls = [];
 /*
 	var res1 = jsonpath(ast, "$.body[0]..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"VALUE"});
 	var res2 = jsonpath(ast, "$.body[0]..[?(@.type=='CallExpression' && @.property !== null && @.property.type=='Identifier' )].property.name", {resultType:"PATH"});*/	
@@ -161,6 +161,58 @@ function analyzeCode(code)
 	var res2 = jsonpath(ast, "$..[?(@.type=='CallExpression' && @.callee !== null)]", {resultType:"PATH"});	
 	console.log('done 3');
 
+	var res3 = jsonpath(ast, "$..[?(@.type=='AssignmentExpression' && @.right!== null && @.right.type == 'FunctionExpression' && @.right.callee !== null && @.right.callee.name == 'require')]", {resultType:"VALUE"});
+	var res4 = jsonpath(ast, "$..[?(@.type=='AssignmentExpression' && @.right!== null && @.right.type == 'FunctionExpression' && @.right.callee !== null && @.right.callee.name == 'require')]", {resultType:"PATH"});	
+
+	var requires = {};
+	for(var item in res3)
+	{
+		var test = res4[item];
+		test = test.slice(1);
+		var array = test.split(']');
+		for(var item in array)
+		{
+			if(isInt(array[item][1]))
+				array[item] = array[item].slice(1);
+			else
+				array[item] = array[item].slice(2, -1);
+		}
+		var astcopy = ast;
+		var leftNodes = [];
+		for(var item in array)
+		{
+			var node = astcopy[array[item]];
+			if(node !== undefined)
+			{
+				if(node.hasOwnProperty('type'))
+				{
+					//console.log(node.type);
+				}
+				astcopy = node;
+			}
+			
+		}
+		if(astcopy.left.type === 'Identifier')
+		{
+			console.log('id : ' + astcopy.left.name + ' : ' + astcopy.right.arguments[0].value);
+			requires[astcopy.left.name] = astcopy.right.arguments[0].value;
+		}
+		else if(astcopy.callee.type === 'MemberExpression')
+		{
+			var mName = visitMemberExpression(astcopy.callee, [])
+			console.log('me : ' + mName + ' : ' + astcopy.right.arguments[0].value);
+			requires[mName] = astcopy.right.arguments[0].value;		
+		}
+		else
+		{
+			console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^' + ' ' + astcopy.callee);
+		}
+		
+		//console.log(astcopy.type);
+		//console.log('-------------------');
+	
+
+	}
 	//console.log(res1[item] + ' : ' + res2[item]);
 	var breakFlag = 0;
 	for(item in res2)
@@ -193,10 +245,13 @@ function analyzeCode(code)
 		if(astcopy.callee.type === 'Identifier')
 		{
 			console.log('id : ' + astcopy.callee.name);
+			methodCalls[methodCalls.length] = astcopy.callee.name;
 		}
 		else if(astcopy.callee.type === 'MemberExpression')
 		{
-			console.log('me : ' + visitMemberExpression(astcopy.callee, []));
+			var mName = visitMemberExpression(astcopy.callee, [])
+			console.log('me : ' + mName);
+			methodCalls[methodCalls.length] = mName;
 		}
 		else
 		{
@@ -205,6 +260,10 @@ function analyzeCode(code)
 		//console.log(astcopy.type);
 		//console.log('-------------------');
 	}
+	var obj = {};
+	obj['requires'] = requires;
+	obj['methodcalls'] = methodCalls;
+	return obj;
 }
 
 
@@ -214,7 +273,7 @@ var data     = fs.readFileSync(filename);
 var esprima  = require('esprima');
 try
 {
-	analyzeCode(data);
+	var methodCalls = analyzeCode(data);
 }
 catch(err)
 {
